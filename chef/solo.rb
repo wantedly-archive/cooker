c6b1a986005a7d1e019887ba5e87eb0adadb3793
Seigo Uchida <spesnova@gmail.com>
2014-02-26 14:33:35 +0900

    Change vendor cookbook and our cookbook path.

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,8 +1,8 @@
 base_path = File.expand_path("..", __FILE__)
 
 cookbook_path []
+cookbook_path << File.join(base_path, "vendor/cookbooks")
 cookbook_path << File.join(base_path, "cookbooks")
-cookbook_path << File.join(base_path, "our-cookbooks")
 
 role_path                 File.join(base_path, "roles")
 nodes_path                File.join(base_path, "nodes")
