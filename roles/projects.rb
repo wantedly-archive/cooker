c535134f9bf3b5f3c9eb68819e8daf517cb2b232
Seigo Uchida <spesnova@gmail.com>
2014-02-26 18:38:15 +0900

    Create projects role for wantedly.

--- roles/projects.rb
+++ roles/projects.rb
@@ -1,17 +1,37 @@
 name "projects"
 description "Run list for each project."
 run_list(
-  "recipe[homebrew]",        # Don't remove this
-  "recipe[homebrew::bundle]" # Don't remove this
-  # Add recipes to run here
+  "recipe[homebrew]",         # Don't remove this
+  "recipe[homebrew::bundle]", # Don't remove this
+  "recipe[os_x::ruby]"
 )
 override_attributes({
   "homebrew" => {
     "formulas" => [
-      # Add formulas to install here
+      #
+      # basic formulas
+      #
+      "git",
+      "hub",
+      "bash-completion",
+      "ruby-build",
+      "rbenv",
+      "heroku-toolbelt",
+      #
+      # for wantedly/wantedly
+      #
+      "imagemagick",
+      "phantomjs",
+      "openssl",
+      "readline"
     ],
     "casks" => [
-      # Add casks to install here
+      "google-chrome",
+      "hipchat",
+      "skitch",
+      "evernote",
+      "dropbox",
+      "dropbox-encore"
     ]
   }
 })
