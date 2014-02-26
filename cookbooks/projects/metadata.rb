2d5b6792fd2c148c7eff03985bf3ca265c3bb391
Seigo Uchida <spesnova@gmail.com>
2014-02-26 19:59:33 +0900

    Include ruby_build recipe to project role.

--- cookbooks/projects/metadata.rb
+++ cookbooks/projects/metadata.rb
@@ -5,4 +5,4 @@ license          'Apache 2.0'
 description      'Installs/Configures projects'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
+depends          'ruby_build'
--- roles/projects.rb
+++ roles/projects.rb
@@ -3,6 +3,7 @@ description "Run list for each project."
 run_list(
   "recipe[homebrew]",         # Don't remove this
   "recipe[homebrew::bundle]", # Don't remove this
+  "recipe[ruby_build]",
   "recipe[os_x::ruby]"
 )
 override_attributes({
