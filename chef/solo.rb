968317138628bd0678e345b2f8626287604052ee
Seigo Uchida <spesnova@gmail.com>
2014-02-26 17:44:50 +0900

    Create cookbooks and roles directory that user modify.

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,10 +1,15 @@
+repo_path = File.expand_path("../..", __FILE__)
 base_path = File.expand_path("..", __FILE__)
 
 cookbook_path []
 cookbook_path << File.join(base_path, "vendor/cookbooks")
 cookbook_path << File.join(base_path, "cookbooks")
+cookbook_path << File.join(base_path, "cookbooks")
+cookbook_path << File.join(repo_path, "cookbooks")
 
-role_path                 File.join(base_path, "roles")
+role_path                 []
+role_path              << File.join(base_path, "roles")
+role_path              << File.join(repo_path, "roles")
 nodes_path                File.join(base_path, "nodes")
 data_bag_path             File.join(base_path, "data_bag")
 encrypted_data_bag_secret File.join(base_path, "data_bag_key")
