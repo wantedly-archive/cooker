3e0aa4f28e0ed2dd23ec1af9f189e8161c3ccaf6
Seigo Uchida <spesnova@gmail.com>
2014-02-26 18:29:49 +0900

    Change default run_list.

--- .chef/roles/default.rb
+++ .chef/roles/default.rb
@@ -1,16 +1,6 @@
 name "default"
 description "Default run list"
 run_list(
-  "recipe[homebrew]",
-  "recipe[homebrew::bundle]"
 )
 override_attributes({
-  "homebrew" => {
-    "formulas" => [
-      "git",
-    ],
-    "casks" => [
-      "google-chrome"
-    ]
-  }
 })
--- roles/projects.rb
+++ roles/projects.rb
@@ -1,6 +1,17 @@
 name "projects"
 description "Run list for each project."
 run_list(
+  "recipe[homebrew]",        # Don't remove this
+  "recipe[homebrew::bundle]" # Don't remove this
+  # Add recipes to run here
 )
 override_attributes({
+  "homebrew" => {
+    "formulas" => [
+      # Add formulas to install here
+    ],
+    "casks" => [
+      # Add casks to install here
+    ]
+  }
 })
