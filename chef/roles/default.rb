5fd285b6e4f63abbbc4434865cf60a6f1dc4a03b
Seigo Uchida <spesnova@gmail.com>
2014-02-26 11:08:56 +0900

    Add brew bundle recipe.

--- chef/roles/default.rb
+++ chef/roles/default.rb
@@ -1,5 +1,21 @@
 name "default"
 description "Default run list"
+override_attributes({
+  "homebrew" => {
+    "formulas" => [
+      "git",
+      "hub",
+      "bash-completion",
+      "ruby-build",
+      "rbenv",
+      "wget",
+      "jq",
+      "tree",
+      "icu4c"
+    ]
+  }
+})
 run_list(
-  "recipe[homebrew]"
+  "recipe[homebrew]",
+  "recipe[homebrew::bundle]"
 )
